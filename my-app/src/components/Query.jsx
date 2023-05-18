f3ecc0a94b39f6ac550c1dbedea4debc1637e3d0
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-18 21:12:41 +0800

    fixing dependency array warning

--- my-app/src/components/Query.jsx
+++ my-app/src/components/Query.jsx
@@ -10,38 +10,37 @@ const Query = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data: cities, error: citiesError } = await supabase
+          .from("cities")
+          .select("name, country_id");
+
+        const { data: countries, error: countriesError } = await supabase
+          .from("countries")
+          .select("id, name, continent_id");
+
+        const { data: continents, error: continentsError } = await supabase
+          .from("continents")
+          .select("id, name");
+
+        if (citiesError || countriesError || continentsError) {
+          console.error(
+            "Error executing queries:",
+            citiesError,
+            countriesError,
+            continentsError
+          );
+        } else {
+          const joinedData = joinData(cities, countries, continents);
+          setData(joinedData);
+        }
+      } catch (error) {
+        console.error("Error retrieving data:", error);
+      }
+    };
     getData();
   }, []);
-
-  const getData = async () => {
-    try {
-      const { data: cities, error: citiesError } = await supabase
-        .from("cities")
-        .select("name, country_id");
-
-      const { data: countries, error: countriesError } = await supabase
-        .from("countries")
-        .select("id, name, continent_id");
-
-      const { data: continents, error: continentsError } = await supabase
-        .from("continents")
-        .select("id, name");
-
-      if (citiesError || countriesError || continentsError) {
-        console.error(
-          "Error executing queries:",
-          citiesError,
-          countriesError,
-          continentsError
-        );
-      } else {
-        const joinedData = joinData(cities, countries, continents);
-        setData(joinedData);
-      }
-    } catch (error) {
-      console.error("Error retrieving data:", error);
-    }
-  };
 
   const joinData = (cities, countries, continents) => {
     return cities.map((city) => {
