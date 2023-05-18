62cb042247847e710bf36ed80a29870badce624f
Chris Anderson <chris.d.anderson87@gmail.com>
2023-05-18 21:10:00 +0800

    had some help from chatgpt but successfully joined the data and displayed

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,20 +1,8 @@
-// import Continents from "./components/Continents";
-// import Countries from "./components/Countries";
-// import Cities from "./components/Cities";
 import Query from "./components/Query";
 
 const App = () => {
   return (
     <>
-      {/* <div>
-        <Continents />
-      </div>
-      <div>
-        <Countries />
-      </div>
-      <div>
-        <Cities />
-      </div> */}
       <Query />
     </>
   );
